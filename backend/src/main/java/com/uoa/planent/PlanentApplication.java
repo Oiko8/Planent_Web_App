205d7b6d449b14185ed741d75258ccfa19812659
Anthony Zacharakis <antonyzah05@gmail.com>
2026-04-19 19:02:49 +0300

    changed backend to java springboot

---
+++ backend/src/main/java/com/uoa/planent/PlanentApplication.java
@@ -0,0 +1,13 @@
+package com.uoa.planent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/uoa/planent/PlanentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoa.planent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
